dcb526b6490780355a6332e1eb4a9b2c8575b70b
Micah456 <98129716+Micah456@users.noreply.github.com>
2024-01-13 15:37:44 +0000

    update 130124

--- OnlineStoreAPIs/SYSAPI_OS_USER.py
+++ OnlineStoreAPIs/SYSAPI_OS_USER.py
@@ -8,7 +8,10 @@ users_table = os.getenv("users_table")
 
 conn_str = "Driver=SQL Server;Server=" + server + ";Database=" + database + ";Trusted_Connection=yes;"
 
+
 def create_user_dict(row):
+    '''Generate a user dictionary from the row data returned from the database'''
+    print("Generating user dict")
     return {
         "ID": row[0],
         "First Name": row[1],
@@ -25,12 +28,14 @@ def create_user_dict(row):
     }
 
 def collate_user_dicts(data):
+    '''Create an user dictionary array from data returned from the database'''
     users_array = []
     for row in data:
         users_array.append(create_user_dict(row))
     return users_array
 
 def get_users(id=None):
+    '''Returns user dictionary from database by id. Returns an array of all user dictionaries if no id given'''
     try:
         cnxn = pyodbc.connect(conn_str)
         cursor = cnxn.cursor()
@@ -56,10 +61,11 @@ def get_users(id=None):
         return None
     
 def get_user_by_email(email):
+    '''Returns user dictionary based on email address'''
     try:
         cnxn = pyodbc.connect(conn_str)
         cursor = cnxn.cursor()
-        statement = "SELECT * FROM " + users_table + "WHERE 'Email Address' = " + email
+        statement = "SELECT * FROM " + users_table + " WHERE EmailAddress = '" + email + "'"
         print(statement)
         cursor.execute(statement)
         data = cursor.fetchall()
@@ -71,6 +77,7 @@ def get_user_by_email(email):
         return None
 
 def create_user(user_dict):
+    '''Adds a user to the database using data from a user dictionary'''
     try:
         values = str(list(user_dict.values()))
         statement = "INSERT INTO " + users_table + "([FirstName], [LastName], [UserName], [Password], [EmailAddress], [DOB], [HouseNumName], [Street], [City], [Country], [PostCode]) VALUES (" + values[1:len(values)-1] + ")"
@@ -86,10 +93,27 @@ def create_user(user_dict):
 
 
 def create_users_from_array(user_dict_array):
+    '''Creates users in the database based on data from an array of user dictionaries'''
     for user_dict in user_dict_array:
         create_user(user_dict)
 
+def delete_user_by_id(id):
+    '''Deletes a user from the database using the user id'''
+    try:
+        statement = "DELETE FROM " + users_table + " WHERE ID = " + str(id)
+        cnxn = pyodbc.connect(conn_str)
+        cursor = cnxn.cursor()
+        cursor.execute(statement)
+        cnxn.commit()
+        return True
+    except Exception as e:
+        print(e)
+        print("Error: could not delete user")
+        return False
+
+
 if __name__ == '__main__':
+    print("Should not be running as main")
     #print("Get all users:")
     #print(get_users())
     #print("Get user id 2")
@@ -97,3 +121,4 @@ if __name__ == '__main__':
     #print("create user")
     #user1 = {'First Name': 'Micah', 'Last Name': 'Chuku', 'Username': 'mchuku', 'Password': 'pass1', 'Email Address': 'mchuku@onlinestore.com', 'DOB': '1998-07-24', 'House NumName': '1', 'Street': 'Generic Street', 'City': 'Generic City', 'Country': 'UK', 'Post Code': 'UK12 G34'}
     #print(create_user(user1))
+    
--- OnlineStoreAPIs/testSYS-OS-USER.py
+++ OnlineStoreAPIs/testSYS-OS-USER.py
@@ -4,21 +4,50 @@ class TestServer(unittest.TestCase):
 
     user1 = {'ID': 2, 'First Name': 'Micah', 'Last Name': 'Chuku', 'Username': 'mchuku', 'Password': 'pass1', 'Email Address': 'mchuku@onlinestore.com', 'DOB': '1998-07-24', 'House NumName': '1', 'Street': 'Generic Street', 'City': 'Generic City', 'Country': 'UK', 'Post Code': 'UK12 G34'}
     user2 = {'First Name': 'Marlee', 'Last Name': 'Vaughn', 'Username': 'mvaughn', 'Password': 'pass2', 'Email Address': 'mvaughn@gmail.com', 'DOB': '2006-03-16', 'House NumName': '2', 'Street': 'Generic Street', 'City': 'Generic City', 'Country': 'UK', 'Post Code': 'UK12 G34'}
+    user3 = {'First Name': 'Duane', 'Last Name': 'Vaughn', 'Username': 'dvaughn', 'Password': 'pass3', 'Email Address': 'dvaughn@hotmail.com', 'DOB': '1998-05-09', 'House NumName': '2', 'Street': 'Generic Street', 'City': 'Generic City', 'Country': 'UK', 'Post Code': 'UK12 G34'}
     def test_get_users(self):
         assert sysUser.get_users() == [self.user1]
+
     def test_get_user_by_ID(self):
         assert sysUser.get_users(self.user1.get('ID')) == self.user1
+
     def test_get_user_by_email(self):
         assert sysUser.get_user_by_email(self.user1.get('Email Address')) == self.user1
-    '''def test_create_user(self):
+
+    def test_create_user(self):
         assert sysUser.get_user_by_email(self.user2.get('Email Address')) == None
         sysUser.create_user(self.user2)
-        assert sysUser.get_user_by_email(self.user2.get('Email Address')) == self.user2'''
-        #Must delete newly created user before running test
+        found_user = sysUser.get_user_by_email(self.user2.get('Email Address'))
+        sysUser.delete_user_by_id(found_user.get('ID'))
+        found_user.pop("ID")
+        assert found_user == self.user2
+        
     def test_create_users_from_array(self):
         #TODO - create, find, then delete new users
+        user_array = [self.user2, self.user3]
+        total_users = len(sysUser.get_users())
+        assert sysUser.get_user_by_email(self.user2.get('Email Address')) == None
+        assert sysUser.get_user_by_email(self.user3.get('Email Address')) == None
+        sysUser.create_users_from_array(user_array)
+        assert len(sysUser.get_users()) == total_users + 2
+        found_user1 = sysUser.get_user_by_email(self.user2.get('Email Address'))
+        found_user2 = sysUser.get_user_by_email(self.user3.get('Email Address'))
+        sysUser.delete_user_by_id(found_user1.get('ID'))
+        sysUser.delete_user_by_id(found_user2.get('ID'))
+        
+    def test_delete_user(self):
+        assert sysUser.get_user_by_email(self.user2.get('Email Address')) == None
+        total_users = len(sysUser.get_users())
+        sysUser.create_user(self.user2)
+        assert len(sysUser.get_users()) == total_users + 1
+        user_data = sysUser.get_user_by_email(self.user2.get('Email Address'))
+        assert sysUser.delete_user_by_id(user_data.get('ID')) == True
+        assert len(sysUser.get_users()) == total_users
+        assert sysUser.get_users(user_data.get('ID')) == None
+
+    #TODO also to do: update user details
+    def test_update_user(self):
         pass
-    #TODO also to do: update user details, delete user, etc.
 
 
 
